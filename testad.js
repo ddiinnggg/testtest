ac526426635589eb343d8995f8b6f87297e8d52c
ddiinnggg <yyyy23.19@ichat.sp.edu.sg>
2020-04-25 17:40:05 +0800

    ad fin

--- testad.js
+++ testad.js
@@ -1,5 +1,6 @@
 class option{
-    constructor(cid, aud, cost, fixed){
+    constructor(opid, cid, aud, cost, fixed){
+        this.opid = opid;
         this.cid = cid;
         this.aud = aud;
         this.cost = cost;
@@ -8,7 +9,8 @@ class option{
 }
 
 function bestop(arr, budget){
-    let weights = [];
+    let result = [];
+    let newarr = [];
     // remove shit
     arr.forEach((e, i) => {
         if(e.fixed){
@@ -18,9 +20,29 @@ function bestop(arr, budget){
         }
     });
     arr.forEach((e) => {
-        weights.push(e.aud/e.cost);
+        newarr.push([e.aud/e.cost, e]);
     });
-    return weights;
+    newarr.sort((a, b) => (b[0]-a[0]));
+    console.log(newarr)
+    newarr.forEach(([w, e]) => {
+        let res = {};
+        let ncost = 0;
+        if (e.fixed){
+            ncost = Math.floor(budget / e.cost) >= 1 ? e.cost : null;
+        }
+        else{
+            let part = budget / e.cost;
+            ncost = part >= 1 ? e.cost : e.cost * part;
+        }
+        if (ncost){
+            budget -= ncost;
+            res["opid"] = e.opid;
+            res["aud"] = ncost * w;
+            res["cost"] = ncost;
+            result.push(res);
+        }
+    })
+    return result;
 }
 
-console.log(bestop([new option(1, 10000, 25, 0), new option(1, 12000, 20, 1), new option(2, 5000, 10, 1), new option(3, 8000, 30, 0)], 19))
+console.log(bestop([new option(1, 1, 10000, 25, 0), new option(2, 1, 12000, 20, 1), new option(3, 2, 5000, 10, 1), new option(4, 3, 8000, 30, 0)], 52))
